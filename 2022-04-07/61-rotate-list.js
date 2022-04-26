abcc31ee8f6f602ac46a420f55a1bbf02c87d1ef
wangleixxxy <wangleixxxy@163.com>
2022-04-26 09:05:15 +0800

    day7

--- 2022-04-07/61-rotate-list.js
+++ 2022-04-07/61-rotate-list.js
@@ -1,32 +1,32 @@
 /**
  * 61. 旋转链表
  */
- var rotateRight = function(head, k) {
-  if (!head) return head
-  let len = 0
-  let dummy = new ListNode()
-  dummy.next = head
-  let f = s = head
-  while (f) {
-      f = f.next
-      len++
-  }
-  k = k % len
-  if (k === 0) return head
+var rotateRight = function (head, k) {
+    if (!head) return head
+    let len = 0
+    let dummy = new ListNode()
+    dummy.next = head
+    let f = s = head
+    while (f) {
+        f = f.next
+        len++
+    }
+    k = k % len
+    if (k === 0) return head
 
-  f = s = head
-  while (k) {
-      f = f.next
-      k--
-  }
-  while (f && f.next) {
-      f = f.next
-      s = s.next
-  }
+    f = s = head
+    while (k) {
+        f = f.next
+        k--
+    }
+    while (f && f.next) {
+        f = f.next
+        s = s.next
+    }
 
-  let p = s.next
-  s.next = null
-  f.next = head
-  dummy.next = p
-  return dummy.next
+    let p = s.next
+    s.next = null
+    f.next = head
+    dummy.next = p
+    return dummy.next
 };
